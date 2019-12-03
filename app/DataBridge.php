1bd6700a70a6b5771e724d2b0becb64b4dd54a05
ineon73 <ineon73@gmail.com>
2019-12-04 00:17:52 +0300

    add new method filter (process)

--- app/DataBridge.php
+++ app/DataBridge.php
@@ -19,11 +19,12 @@ class DataBridge extends Corsel
                     if (isset($data['modifiedFrom'])) {
                         $a->whereDate('post_modified', '>=', $data['modifiedFrom']);
                     }
-                    if (isset($data['gateway'])){
+                    if (isset($data['gateway'])) {
                         $a->hasMeta(['leyka_gateway' => $data['gateway']]);
                     }
                     if (isset($data['status'])) {
-                        $a->where('post_status', '=', $data['status']); }
+                        $a->where('post_status', '=', $data['status']);
+                    }
                     return $a->get();
                 } else {
                     return $this->getSomeById($data);
@@ -47,11 +48,65 @@ class DataBridge extends Corsel
         foreach ($ids as $key => $id) {
             $data[$key] = Corsel::find($id);
         }
-        return $data;
+        return $this->filterData($data);
     }
 
-    protected
-        $casts = [
+    public function filterData($callback)
+    {
+        $fields = [
+            'email' => 'Array',
+            'phone' => 'Array',
+            'bin' => 'String',
+            'ewallet' => 'String',
+            'campaign_id' => 'integer',
+            'campaign_name' => 'String',
+            'acquirer_id' => 'String',
+            'site_id' => 'String',
+            'full_name' => 'String',
+            'acquirer_name' => 'String',
+            'comment' => 'String',
+            'summa_cur_net' => 'double',
+            'summa_cur_gross' => 'double',
+            'currency_code' => 'String',
+            'summa_rur_gross' => 'double',
+            'summa_rur_net' => 'double',
+            'recurring' => 'bool',
+            'recurring_id' => 'String',
+            'recurring_first' => 'integer',
+            'utm_campaign' => 'String',
+            'utm_source' => 'String',
+            'status' => 'String',
+            'address' => 'String',
+            'signout' => 'bool',
+            'raw json' => 'json',
+            'reject_reason' => 'String',
+            'warning_message' => 'String',
+            'upload_id' => 'String',
+        ];
+
+echo "<hr>";
+
+    echo $relevant['email']  = $callback['id']->leyka_donor_email;
+     echo   $relevant['full_name']  = $callback['id']->leyka_donor_name;
+     echo   $relevant['recurring_id'] = $callback['id']->cp_recurring_id;
+        echo   $relevant['status'] = $callback['id']->post_status;
+        echo   $relevant['campaign_id'] = $callback['id']->leyka_campaign_id;
+        echo   $relevant['currency_code '] = $callback['id']->leyka_donation_currency;
+        echo   $relevant['recurring_id'] = $callback['id']->cp_recurring_id;
+        echo   $relevant['recurring_id'] = $callback['id']->cp_recurring_id;
+        echo   $relevant['recurring_id'] = $callback['id']->cp_recurring_id;
+        echo   $relevant['recurring_id'] = $callback['id']->cp_recurring_id;
+        echo   $relevant['recurring_id'] = $callback['id']->cp_recurring_id;
+        echo   $relevant['recurring_id'] = $callback['id']->cp_recurring_id;
+        echo   $relevant['recurring_id'] = $callback['id']->cp_recurring_id;
+        echo   $relevant['recurring_id'] = $callback['id']->cp_recurring_id;
+        echo   $relevant['recurring_id'] = $callback['id']->cp_recurring_id;
+
+    }
+
+
+
+    protected  $casts = [
         'post_author' => 'integer',
         'post_content' => 'string',
         'post_title' => 'string',
--- app/Http/Controllers/DataAccess.php
+++ app/Http/Controllers/DataAccess.php
@@ -13,6 +13,6 @@ class DataAccess extends Controller
         echo "<hr>";
         $mod = new Model();
         //dd($mod->get($array));
-        var_dump($mod->get($data));
+        $mod->get($data);
     }
 }
